d9ea3f39c33d42081fe1f8ea05e9554d4da288bf
Mohammad Anas <mohammadanas702@gmail.com>
2023-06-13 18:34:52 +0500

    Before going home

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,48 @@
 class InventoriesController < ApplicationController
-end
+    def index
+      @inventories = Inventory.all
+    end
+  
+    def new
+      @inventory = Inventory.new
+    end
+  
+    def create
+      @inventory = Inventory.new(inventory_params)
+      if @inventory.save
+        redirect_to @inventory, notice: 'Inventory item was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @inventory = Inventory.find(params[:id])
+    end
+  
+    def edit
+      @inventory = Inventory.find(params[:id])
+    end
+  
+    def update
+        @inventory = Inventory.find(params[:id])
+        if @inventory.update(inventory_params)
+            redirect_to @inventory, notice: 'Inventory item was successfully updated.'
+        else
+            render :edit
+      end
+    end
+  
+    def destroy
+      @inventory = Inventory.find(params[:id])
+      @inventory.destroy
+      redirect_to inventories_url, notice: 'Inventory item was successfully deleted.'
+    end
+  
+    private
+  
+    def inventory_params
+      params.require(:inventory).permit(:name, :description, :quantity)
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :inventories
+  resources :inventories
 
-  # root "inventories#new"
+  root "homes#index"
+  get '/view/inventories', to: 'inventories#index', as: 'view_inventories'
+  # delete
+  # get '/inventories/:id', to: 'inventories#show', as: 'inventory'
+  # get '/inventories/:id/edit', to: 'inventories#edit', as: 'edit_inventory'
+  # patch '/inventories/:id', to: 'inventories#update'
+  delete '/inventories/:id', to: 'inventories#destroy'
+
+
 end
